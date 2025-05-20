351fa10ea65b3785be7a0276eebe9913c40877b7
Igor <igorcat100@gmail.com>
2025-05-20 14:33:29 +0300

    upd: added dashboard auto load in startup

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# 1. Создаём ConfigMap из файла
+kubectl create configmap grafana-my-dashboard \
+  --from-file=global-dashboard.json \
+  -n hse-coursework-health
+
+# 2. Добавляем нужный лейбл
+kubectl label configmap grafana-my-dashboard \
+  grafana_dashboard=1 \
+  -n hse-coursework-health
+
+
 helm repo add grafana https://grafana.github.io/helm-charts
 
 helm install grafana grafana/grafana -n hse-coursework-health -f values.yaml
--- stop.sh
+++ stop.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-helm delete grafana -n hse-coursework-health
+kubectl delete configmap grafana-my-dashboard \
+  -n hse-coursework-health
+
+helm delete grafana -n hse-coursework-health || true
+helm uninstall grafana -n hse-coursework-health || true
+helm repo remove grafana || true
+kubectl delete -f values.yaml -n hse-coursework-health || true
+kubectl delete pvc -l app.kubernetes.io/instance=grafana -n hse-coursework-health || true
+kubectl delete all,configmap,secret --selector=app.kubernetes.io/instance=grafana -n hse-coursework-health || true
