7b168be2ebbc1298db00009baa731a701aa417e1
Igor <igorcat100@gmail.com>
2025-06-21 17:10:01 +0300

    added readme

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# 1. Создаём ConfigMap из файла
 kubectl create configmap grafana-my-dashboard \
   --from-file=global-dashboard.json \
   -n hse-coursework-health
 
-# 2. Добавляем нужный лейбл
 kubectl label configmap grafana-my-dashboard \
   grafana_dashboard=1 \
   -n hse-coursework-health
